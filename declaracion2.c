42fffcbd03f9de8cc7e0d1346354b07ccf64260f
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-19 17:22:53 -0600

    Add files via upload

---
+++ declaracion2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int minS = 60;
+    int horaM =60;
+    int diaH = 24;
+    int anioD = 365;
+    int mult = minS * horaM * diaH * anioD;
+     printf ("%f" , mult);
+     printf("\n");
+     return 0;
+
+}
